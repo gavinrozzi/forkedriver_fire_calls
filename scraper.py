660fd1a98f07204a27158103e3331cd8a44d43d9
Gavin Rozzi <gr@gavinrozzi.com>
2018-01-07 23:51:39 -0500

    Add XPath query

--- scraper.py
+++ scraper.py
@@ -1,7 +1,7 @@
 # This is a Python scraper that saves Forked River Fire Department calls for service. Copyright (C) 2017 Gavin Rozzi
 
 # # Import dependencies
-from lxml import html
+import lxml
 import requests
 import scraperwiki
 
@@ -10,6 +10,6 @@ page = requests.get('http://www.forkedriverfire.com/firecallsstats.htm')
 tree = html.fromstring(page.content)
 
 ## Select calls from page by XPath
-
+call = tree.xpath('//div//p/text()')
 # # Write out to the sqlite database using scraperwiki library
 scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
