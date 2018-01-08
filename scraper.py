8e0684a1cc222375565e03614152cdd688096b9e
Gavin Rozzi <gr@gavinrozzi.com>
2018-01-07 23:05:52 -0500

    Initial commit

--- scraper.py
+++ scraper.py
@@ -1,24 +1,15 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+# This is a Python scraper that saves Forked River Fire Department calls for service. Copyright (C) 2017 Gavin Rozzi
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
+# # Import dependencies
+from lxml import html
+import requests
+import scraperwiki
+
+# # Read in the FRFD call page
+page = requests.get('http://www.forkedriverfire.com/firecallsstats.htm')
+tree = html.fromstring(page.content)
+
+## Select calls from page by XPath
+
 # # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
-
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
