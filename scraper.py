69cc35cf8d015b04fc5b396dc128bf45a877393b
Gavin Rozzi <gr@gavinrozzi.com>
2018-01-08 00:03:24 -0500

    Update scraper logic

--- scraper.py
+++ scraper.py
@@ -1,15 +1,17 @@
-# This is a Python scraper that saves Forked River Fire Department calls for service. Copyright (C) 2017 Gavin Rozzi
+## This is a Python scraper that saves Forked River Fire Department calls for service. Copyright (C) 2017 Gavin Rozzi
 
-# # Import dependencies
+## Import dependencies
 import lxml
 import requests
 import scraperwiki
 
-# # Read in the FRFD call page
+## Read in the FRFD call page
 page = requests.get('http://www.forkedriverfire.com/firecallsstats.htm')
 tree = html.fromstring(page.content)
 
 ## Select calls from page by XPath
 call = tree.xpath('//div//p/text()')
-# # Write out to the sqlite database using scraperwiki library
-scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
+
+## Write out to the sqlite database using scraperwiki library
+scraperwiki.sqlite.save(unique_keys=['Forked River Fire Calls'], data={"call"})
+
