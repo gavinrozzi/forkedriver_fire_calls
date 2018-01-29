ebe2b6e25075d2cfbfc388444d654865ae8c750f
Gavin Rozzi <gr@gavinrozzi.com>
2018-01-28 21:23:37 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 ## This is a Python scraper that saves Forked River Fire Department calls for service. Copyright (C) 2018 Gavin Rozzi
 
 ## Import dependencies
+from bs4 import BeautifulSoup
 import lxml
 from lxml import html
 import requests
@@ -17,8 +18,8 @@ firecalls = tree.xpath('//div//p/text()')
 data = {
         'firecalls': 'firecalls'     
 }
-
+timestamp = date_parse(u"%s %s" % (date, time))
 print data
 
 ## Write out to the sqlite database using scraperwiki library
-scraperwiki.sqlite.save(unique_keys=['data'], data={"firecalls"})
+scraperwiki.sqlite.save(unique_keys=['timestamp'], data={"firecalls"})
